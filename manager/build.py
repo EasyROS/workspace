119aa6e3b7ad4ecdfbd5263715c13d0325583ad9
LsDeanXz <1328788930@qq.com>
2018-08-29 10:07:17 +0800

    build update

--- manager/build.py
+++ manager/build.py
@@ -3,7 +3,7 @@ import os
 
 def run(arg):
     print 'Build'
-    str = arg.split(":", 1)[-1]
+    str = [arg.split(':', 1)[-1], 'release'][len(arg.split(":", 1)) == 1]
     os.system('catkin build --force-color --profile ' + str)
 
 
