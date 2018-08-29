c9bd4bd4eead6a248ea6c978e483ac520a40121f
LsDeanXz <1328788930@qq.com>
2018-08-29 10:17:07 +0800

    clean update

--- manager/clean.py
+++ manager/clean.py
@@ -2,10 +2,12 @@ import os
 
 
 def run(arg):
-    print 'Build'
-    os.system('catkin clean --yes --profile release')
+    print 'Clean'
+    str = [arg.split(':', 1)[-1], 'release'][len(arg.split(":", 1)) == 1]
+    os.system('catkin clean --yes --profile ' + str)
 
 
 def exe(arg):
-    print 'Build'
-    os.system('catkin build --yes --profile release ' + arg)
+    print 'Clean'
+    print 'Do not surplus options'
+    # os.system('catkin build --yes --profile release ' + arg)
