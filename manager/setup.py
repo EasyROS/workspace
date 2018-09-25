d8a98eb8b56b0a96af405017081e468b2d20762f
LsDeanXz <1328788930@qq.com>
2018-09-25 13:08:32 +0800

    update setup

--- manager/setup.py
+++ manager/setup.py
@@ -12,6 +12,7 @@ def run(arg):
     os.system('catkin config --profile release -x _release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-msse2"')
     os.system('catkin config --profile novision -x _nv_release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-msse2" -DBUILD_KID_SIZE_VISION=OFF')
     os.system('catkin config --profile novision_debug -x _nv_debug --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-msse2" -DBUILD_KID_SIZE_VISION=OFF')
+    os.system('catkin config --profile test -x _test --cmake-args -DCMAKE_BUILD_TYPE=Release -DTest=ON')
 
 
 def exe(arg):
